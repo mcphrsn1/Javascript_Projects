d997b9019dd85404209dc12e86d155a85c2cc415
austinmac95 <anthonymacpherson95@gmail.com>
2022-01-11 11:05:42 -0800

    js

---
+++ alert.js
@@ -0,0 +1,8 @@
+var welcome = "I said, \"Welcome to my site.\"" + " and in return, you said, \"Thank you.\"";
+window.alert(welcome);
+
+var europe = "Spain, Portugal, Switzerland", asia = "Thailand, Japan, India", sAmerica = "Peru, Argentina, Chile";
+var europe = europe.fontcolor(blue); //I'm finding that this is a deprecated function, however, I can't seem to find it's replacement?
+document.write(europe);
+
+addThree = 3 + 3;
