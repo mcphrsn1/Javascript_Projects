188bfaf2bb4f400a29c4ead505f13f0c6bfbfe54
austinmac95 <anthonymacpherson95@gmail.com>
2022-01-23 09:11:51 -0800

    Dictionary

---
+++ basic-js-projects/project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function my_dictionary() {
+    var Wine = {
+        Varietal: "Pinot Noir",
+        Year: "2014",
+        Color: "Ruby",
+        Notes: "Charcoal, Strawberry, Pencil Shavings",
+        Viscosity: "Light"
+    };
+    document.getElementById("Dictionary").innerHTML = Wine;
+}
