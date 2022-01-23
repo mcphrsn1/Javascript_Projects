52697788f6930c12e0fbb858437720bb0bb2718e
austinmac95 <anthonymacpherson95@gmail.com>
2022-01-23 08:56:26 -0800

    Js math operations

---
+++ basic-js-projects/project3_math_operations/JS/main.js
@@ -0,0 +1,40 @@
+function multiply(a,b) {
+    return a * b;
+}
+
+var x = multiply(7,154);
+
+function subtraction(a,b) {
+    return a - b;
+}
+var sub = subtraction(430,79);
+
+function addition(a,b) {
+    return a + b;
+}
+var add = addition(5,7);
+
+function division(a,b) {
+    return a / b;
+}
+var divide = division(100678,478);
+
+var all_math = (10 * 7) / 4 + 2;
+
+var modulus = 25 % 6;
+
+var x = 10;
+
+var X = 3.4;
+X--;
+document.write(X);
+
+var Y = 7;
+Y++;
+window.alert(Y);
+
+window.alert(Math.random() * 1000);
+
+function math1(a,b) {
+    return Math.sqrt(a * b);
+}
